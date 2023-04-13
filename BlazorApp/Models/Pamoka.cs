0caa22715cd4d36d2b20d0a279b4df516d76eb01
Pijus-N <75778499+Pijus-N@users.noreply.github.com>
2023-04-13 12:22:32 +0300

    Mokytojo initial posistemė

---
+++ BlazorApp/Models/Pamoka.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BlazorServer.Models
+{
+    public class Pamoka
+    {
+        public int id { get; set; }
+        [Required]
+        [StringLength(20, ErrorMessage = "Per ilga pavardė.")]
+        public string instrument { get; set; }
+        [Required]
+        public string students_count { get; set; }
+        [Required]
+        public string date { get; set; }
+        
+        public DateTime dateInFormat { get; set; }
+        [Required]
+        public int time { get; set; }
+        [Required]
+        public string type { get; set; }
+        [Required]
+        public string price { get; set; }
+
+    }
+
+}
