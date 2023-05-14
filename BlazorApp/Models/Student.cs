d37b567445b2dfc4eff7a4a178abfae4a3cf5204
goblin <gustonas1@gmail.com>
2023-05-14 21:15:41 +0300

    Fix register, add basic admin page
    Registration now works when registering as a guardian.
    
    a basic admin page letting you view all registered users and not much else

---
+++ BlazorApp/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BlazorServer.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+
+        public int user_id { get; set; }
+    }
+
+}
