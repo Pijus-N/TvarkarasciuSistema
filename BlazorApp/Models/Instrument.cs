74d82581188ffe23bac317b6b32c5d70fb88dea2
Emilis <46862485+Qoldoon@users.noreply.github.com>
2023-05-15 16:57:10 +0300

    Adds instrument list

---
+++ BlazorApp/Models/Instrument.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BlazorServer.Models
+{
+    public class Instrument
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public double rental_price { get; set; }
+        public string instrument_type { get; set; }
+        public string instrument_state { get; set; }
+        public int renter_id { get; set; }
+    }
+
+}
