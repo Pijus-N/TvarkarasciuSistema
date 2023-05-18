4c4d04789138f6842a3c5e124baf7972ced58bb5
goblin <gustonas1@gmail.com>
2023-05-18 23:03:24 +0300

    LessonList almost done

---
+++ BlazorApp/Models/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BlazorServer.Models
+{
+    public class Teacher
+    {
+        public int id { get; set; }
+        public string instrument {get; set;}
+        public string location { get; set;}
+        public double price { get; set;}
+        public double group_price { get; set;}
+        public int user_id { get; set; }
+    }
+
+}
