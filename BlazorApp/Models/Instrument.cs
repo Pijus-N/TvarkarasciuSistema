6c908f5698a8fc9a35446904ad7672249f8ffac7
Emilis <46862485+Qoldoon@users.noreply.github.com>
2023-05-20 15:26:17 +0300

    Add new instrument

--- BlazorApp/Models/Instrument.cs
+++ BlazorApp/Models/Instrument.cs
@@ -6,7 +6,9 @@ namespace BlazorServer.Models
         public int id { get; set; }
         public string name { get; set; }
         public string description { get; set; }
+        [Required]
         public double rental_price { get; set; }
+        [Required]
         public string instrument_type { get; set; }
         public string instrument_state { get; set; }
         public int renter_id { get; set; }
